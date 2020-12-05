43d3171a854f3032f2485943506423c1c1fa5540
manu <manureddy.com>
2020-12-05 21:49:01 +0530

    updated mini project

--- project/server/main.go
+++ project/server/main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"net"
+	"os"
 	"github.com/hashicorp/go-hclog"
 	"google.golang.org/grpc"
+	protos "github.com/manureddy7143/sql/project/protos"
+	"github.com/manureddy7143/sql/project/protos/server"
+	"google.golang.org/grpc/reflection"
+
+
 
 )
 
@@ -10,7 +17,15 @@ func main(){
 
 	log := hclog.Default()
 
-	gs := grpc.Newserver()
+	gs := grpc.NewServer()
+	cs := server.NewCourse(log)
 
-	protos.RegisterCourseService(gs,)
+	protos.RegisterCourseService(gs,cs)
+	reflection.Register(gs)
+	l,err := net.Listen("tcp",":8080")
+	if err !=nil {
+		log.Error{"unable to listen","error",err}
+		os.Exit(1)
+	}
+	gs.Serve(l)
 }
--- project/server/server.go
+++ project/server/server.go
@@ -1,15 +1,22 @@
 package server
 import(
 	"github.com/hashicorp/go-hclog"
-	
-
-
+	protos "github.com/manureddy7143/sql/project/protos"
+	"context"
 )
 
 type Course struct{
 	log hclog.Logger
 }
+func NewCourse(l hclog.Logger) *Currency {
+	return &Course{l}
 
-func (c*Course) GetKey(ctx context.context, *Request) (*proto.Response,error) {
+}  
+	
 
+
+func (c*Course) GetKey(ctx context.context, rr *protos.Request) (*protos.Response,error) {
+ 
+ return &protos.Response{Value:"available",Count=1,Hour="7",Repeat=5},nil
 }
+
